ba9c4698de6a09db8e6aa067837e33cd63180a14
Edgar Trejo <edtrelo18@ciencias.unam.mx>
2023-04-08 14:31:00 -0600

    Ordené los archivos de los casos

--- Modeling COVID-19 Spreading in the ZMVM/data/orderingdata.py
+++
@@ -1,7 +0,0 @@
-# ordenamos los datos para que los municipios se presenten en orden alfabético
-import pandas as pd
-url_casos = "https://raw.githubusercontent.com/edtrelo/BioMatematica/main/Modeling%20COVID-19%20Spreading%20in%20the%20ZMVM/data/cleandata/Casos_Diarios_ZMVM_Confirmados.csv"
-casos = pd.read_csv(url_casos)
-casos.sort_values(by = 'nombre', inplace = True)
-casos.to_csv('D:\\Edgar Trejo\\Universidad\\BioMatematica\\Modeling COVID-19 Spreading in the ZMVM\\data\\cleandata\\Casos_Diarios_ZMVM_Confirmados.csv', 
-             sep = ',', encoding = 'utf-8', index = False)
